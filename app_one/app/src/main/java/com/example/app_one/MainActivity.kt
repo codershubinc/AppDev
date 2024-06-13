cb2b7873a5cf63cf73ab051545a7b317407281ee
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-06-13 22:52:56 +0530

    add ui changes to app

--- app_one/app/src/main/java/com/example/app_one/MainActivity.kt
+++ app_one/app/src/main/java/com/example/app_one/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.app_one
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import com.example.app_one.ui.theme.App_oneTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +33,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             App_oneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier =
+                    Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modif0ier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
@@ -31,17 +49,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+//    View(
+//        names = "re" ,
+//        name = name
+//
+//
+//        )
+
 }
 
-@Preview(showBackground = true)
+
+
+@Preview(
+    showBackground = false,
+    showSystemUi = true,
+    fontScale = 1.0f,
+    backgroundColor = 0x004C4F52,
+    name = "App_one",
+
+
+)
 @Composable
 fun GreetingPreview() {
     App_oneTheme {
-        Greeting("Android")
+        Greeting("Android studio")
+
+
     }
 }
