3f6db04ed059584df5bc73b20ef13c38ce2f34ea
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-06-16 13:21:13 +0530

    image transformation

--- app_Projaect_0/app/build.gradle.kts
+++ app_Projaect_0/app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("io.coil-kt:coil:2.6.0")
 }
--- app_Projaect_0/app/src/main/java/com/example/app_projaect_0/MainActivity.kt
+++ app_Projaect_0/app/src/main/java/com/example/app_projaect_0/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.app_projaect_0
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import coil.load
+
+@SuppressLint("SetTextI18n")
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,29 +26,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-        var textViewAct = findViewById<TextView>(R.id.textView)
+        val imView = findViewById<ImageView>(R.id.imageView)
+        imView.load(
+            "https://tse3.mm.bing.net/th?q=Load+Image+from+URL+in+Android+Using+Glide+Library&pid=Api&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=3"
+        )
+        val textViewAct = findViewById<TextView>(R.id.textView)
         textViewAct.text = buildString {
-        append("text Changed")
-    }
-        textViewAct.setOnClickListener {
-           onClickTheText(it)
+            append("text Changed")
         }
+        textViewAct.setOnClickListener {
+            onClickTheText()
+        }
+
 
     }
 
     private var clickCount = 0
-    private fun onClickTheText(view: View) {
+
+
+    private fun onClickTheText() {
         val textViewAct = findViewById<TextView>(R.id.textView)
+        if (clickCount == 20) {
+            textViewAct.text = "To Many Clicks "
+        }
+
         textViewAct.text = "Text Changed once again $clickCount"
         clickCount++
     }
-     fun resetCounter(view: View){
-        clickCount = 0
-         val textViewAct = findViewById<TextView>(R.id.textView)
-         textViewAct.text = "Text Changed"
-    }
 
+    fun resetCounter(view: View) {
+        clickCount = 0
+        val textViewAct = findViewById<TextView>(R.id.textView)
+        textViewAct.text = "Text Changed"
+    }
 
 
 }
